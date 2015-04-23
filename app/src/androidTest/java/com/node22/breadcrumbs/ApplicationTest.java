f9c90f4487f3b771846c8badb2d819389fd1022b
Zenon Harley <code@zenonharley.com>
2015-04-23 15:37:51 -0400

    Create default Android Studio project with blank activity.

---
+++ app/src/androidTest/java/com/node22/breadcrumbs/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.node22.breadcrumbs;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
